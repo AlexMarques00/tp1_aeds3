ff3c0bf58cbdac81bce2144bf7418ebca58001eb
Alex Marques <alex@recriarsistemas.com.br>
2025-02-18 14:41:16 -0300

    metadados e comecei a escrever os dados no bd

--- ReadCSV.java
+++ ReadCSV.java
@@ -1,17 +1,15 @@
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.DataInputStream;
-import java.io.FileInputStream;
 import java.io.RandomAccessFile;
-import java.util.ArrayList;
 
 public class ReadCSV extends Animes{
     public static void main(String args[]) throws Exception{
-
+        RandomAccessFile arq = new RandomAccessFile("animeDataBase.db", "rw");
+        // int ultima id utilizada no inicio
+        arq.writeInt(18495);
+        arq.close();
         RandomAccessFile csv = new RandomAccessFile("BaseAnimesCsv.csv", "rw");
         csv.readLine();
         String input = csv.readLine();
-        for(int i = 0; input != null; i++){
+        while(input != null){
             System.out.println(input);
             Animes animeTmp = new Animes(input);
             addToDatabase(animeTmp);
@@ -21,20 +19,31 @@ public class ReadCSV extends Animes{
         csv.close();
     }
     public static void addToDatabase(Animes anime) throws Exception{
-        FileOutputStream arq = new FileOutputStream("animeDataBase.db");
-        DataOutputStream dos = new DataOutputStream(arq);
-        
-        // ID,Name,Type,Episodes,Rating,Realease_year,Genres,Release_season,Studio
+        RandomAccessFile arq = new RandomAccessFile("animeDataBase.db", "rw");
+        arq.writeInt(anime.getId());
 
-        // int id;
-        // String name;
-        // String type;
-        // int episodes;
-        // float rating;
-        // int year;
-        // ArrayList<String> genres;
-        // String season;
-        // String studio;
 
+
+
+
+        // para cada objeto:
+        //     short tamanho do objeto
+        //     byte de lápide ('*' para morto e ' ' para vivo)
+        //     int id
+        //     short tamanho do nome
+        //     string nome
+        //     string type (tam = 5 bytes)
+        //     short episodes
+        //     float rating
+        //     short year
+        //     short numero de genres 
+        //     {
+        //         short tamanho do genre
+        //         string genre
+        //     }
+        //     string season (tam = 5 bytes) (if season == "" -> write "     ")
+        //     short tamanho do studio
+        //     string studio
+        arq.close();
     }
 }
